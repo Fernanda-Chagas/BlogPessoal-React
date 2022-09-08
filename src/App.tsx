3b62bd0882818b73ea77ac2d55513726f2d2cfcf
Fernanda Chagas <fernanda.kmchagas@gmail.com>
2022-09-08 00:27:58 -0300

    Adicionando componentes estaticos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Navbar from './Components/estaticos/navbar/Navbar';
+import Footer from './Components/estaticos/footer/Footer';
+import { Grid } from '@material-ui/core';
 import Home from './paginas/home/Home';
 import './App.css';
 
 
 function App() {
   return (
-    <Home/>
+    <>
+     <Navbar/>
+     <Home/>
+     <Footer/>
+    </>
   );
 }
 
